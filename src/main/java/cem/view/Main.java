1ec99c767dfff10158835584a55533ed79e5c8a8
alex <ahernandezguerrero582@gmail.com>
2025-02-27 16:26:51 +0100

    commit trie 1

--- src/main/java/cem/view/Main.java
+++ src/main/java/cem/view/Main.java
@@ -2,6 +2,6 @@ package cem.view;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world ");
+        System.out.println("Hola mundo");
     }
 }
