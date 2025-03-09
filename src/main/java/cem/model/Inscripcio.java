15a15bdcab60b1fca27c5becdc07569331b9cfa9
ivang <ivangarca2005@gmail.com>
2025-03-09 02:57:10 +0100

    .quitar

--- src/main/java/cem/model/Inscripcio.java
+++ src/main/java/cem/model/Inscripcio.java
@@ -87,9 +87,5 @@ public class Inscripcio {
         return ins.getDorsal() == getDorsal();
     }
 
-    @Override
-    public String toString() {
-        String modalitatt = modalitat ? "Llarga" : "Curta";
-        return modalitatt;
-    }
+
 }
