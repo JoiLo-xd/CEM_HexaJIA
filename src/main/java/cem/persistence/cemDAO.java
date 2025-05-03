ab346ada711e227cc148429fa196fd7da5ea9f9f
ivang <ivangarca2005@gmail.com>
2025-05-03 18:07:26 +0200

    cambiar nombre

--- src/main/java/cem/persistence/cemDAO.java
+++ src/main/java/cem/persistence/cemDAO.java
@@ -4,9 +4,11 @@
  */
 package cem.persistence;
 
+import cem.model.Corredor;
 import cem.model.Marxa;
 import cem.model.TO.ParticipantEditionTO;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -19,7 +21,7 @@ import java.util.ArrayList;
  * @author ivang
  */
 public class cemDAO {
-    
+
     private Connection conectar() throws SQLException {
         String url = "jdbc:mysql://localhost:3306/cem";
         String user = "root";
@@ -27,20 +29,11 @@ public class cemDAO {
         Connection c = DriverManager.getConnection(url, user, pass);
         return c;
     }
-    
+
     private void desconectar(Connection c) throws SQLException {
         c.close();
     }
-    
-    public void inserMarxa(Marxa marxa) throws SQLException {
-        Connection c = conectar();
-        PreparedStatement ps = c.prepareStatement("insert into marxa values (?);");
-        ps.setInt(1, marxa.getEdicio());
-        ps.executeUpdate();
-        ps.close();
-        desconectar(c);
-    }
-    
+
     public ArrayList<ParticipantEditionTO> getMarxes() throws SQLException {
         Connection c = conectar();
         Statement st = c.createStatement();
@@ -49,30 +42,89 @@ public class cemDAO {
         while (rs.next()) {
             int edicio = rs.getInt("edicio");
             Statement st2 = c.createStatement();
-            ResultSet rs2 = st2.executeQuery("SELECT COUNT(*) FROM participa WHERE edicio = " + edicio);
+            ResultSet rs2 = st2.executeQuery("SELECT COUNT(*) FROM participants WHERE edicio = " + edicio);
             int numParticipants = 0;
             if (rs2.next()) {
                 numParticipants = rs2.getInt(1);
             }
             rs2.close();
             st2.close();
-        marxes.add(new ParticipantEditionTO(edicio, numParticipants));
+            marxes.add(new ParticipantEditionTO(edicio, numParticipants));
         }
         rs.close();
         st.close();
         desconectar(c);
         return marxes;
     }
-    
+
+    public void insertMarxa(Marxa marxa) throws SQLException {
+        Connection c = conectar();
+        PreparedStatement ps = c.prepareStatement("insert into marxa values (?);");
+        ps.setInt(1, marxa.getEdicio());
+        ps.executeUpdate();
+        ps.close();
+        desconectar(c);
+    }
+
     public boolean existMarxa(Marxa marxa) throws SQLException {
         Connection c = conectar();
         Statement st = c.createStatement();
-        ResultSet rs = st.executeQuery("select * from marxa where edicio = '" + marxa.getEdicio() +"';");
+        ResultSet rs = st.executeQuery("select * from marxa where edicio = '" + marxa.getEdicio() + "';");
         boolean existe = rs.next();
         rs.close();
         st.close();
         desconectar(c);
         return existe;
     }
+
+    public boolean existMarxa2015(int n) throws SQLException {
+        Connection c = conectar();
+        Statement st = c.createStatement();
+        ResultSet rs = st.executeQuery("select * from marxa where edicio = '" + n + "';");
+        boolean existe = rs.next();
+        rs.close();
+        st.close();
+        desconectar(c);
+        return existe;
+    }
+
+    public void insertParticpant(Corredor corredor) throws SQLException {
+        Connection c = conectar();
+        PreparedStatement ps = c.prepareStatement("insert into coche values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
+        ps.setString(1, corredor.getNif());
+        ps.setString(2, corredor.getNom());
+        ps.setString(3, corredor.getCognoms());
+        ps.setDate(4, Date.valueOf(corredor.getDataNaixement()));
+        ps.setBoolean(5, corredor.isSexe());
+        ps.setString(6, corredor.getPoblacio());
+        ps.setString(7, corredor.getNumTelefon());
+        ps.setString(8, corredor.getEmail());
+        if (!corredor.isFederat()) {
+            ps.setNull(9, java.sql.Types.VARCHAR);
+            ps.setNull(10, java.sql.Types.VARCHAR);
+        } else {
+            ps.setBoolean(9, corredor.isFederat());
+            ps.setString(10, corredor.getEntitat());
+        }
+        if (corredor.getObservacions() == null) {
+            ps.setNull(11, java.sql.Types.VARCHAR);
+        } else {
+            ps.setString(8, corredor.getObservacions());
+        }
+        ps.executeUpdate();
+        ps.close();
+        desconectar(c);
+    }
     
+        public boolean existParticpant(Corredor corredor) throws SQLException {
+        Connection c = conectar();
+        Statement st = c.createStatement();
+        ResultSet rs = st.executeQuery("select * from corredor where nif = '" + corredor.getNif() + "';");
+        boolean existe = rs.next();
+        rs.close();
+        st.close();
+        desconectar(c);
+        return existe;
+    }
+
 }
